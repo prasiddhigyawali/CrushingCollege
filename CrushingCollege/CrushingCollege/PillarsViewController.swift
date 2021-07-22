a7849400518e473b848f4478842d6e3c7831217b
prasiddhigyawali <prasiddhigyawali@gmail.com>
2021-07-22 00:21:31 -0700

    pillars added + label rotation

--- CrushingCollege/CrushingCollege/PillarsViewController.swift
+++ CrushingCollege/CrushingCollege/PillarsViewController.swift
@@ -8,9 +8,25 @@
 import UIKit
 
 class PillarsViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var help1: UILabel!
+    
+    @IBOutlet weak var help2: UILabel!
+    
+    @IBOutlet weak var help3: UILabel!
+    
+    @IBOutlet weak var help4: UILabel!
+    
+    @IBOutlet weak var help5: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        help1.transform = CGAffineTransform(rotationAngle: (5 * CGFloat.pi) / 3)
+        help2.transform = CGAffineTransform(rotationAngle: (5 * CGFloat.pi) / 3)
+        help3.transform = CGAffineTransform(rotationAngle: (5 * CGFloat.pi) / 3)
+        help4.transform = CGAffineTransform(rotationAngle: (5 * CGFloat.pi) / 3)
+        help5.transform = CGAffineTransform(rotationAngle: (5 * CGFloat.pi) / 3)
 
         // Do any additional setup after loading the view.
     }
