7d97289dbd2788e1e07d2be9aa26fd95235cada0
prasiddhigyawali <prasiddhigyawali@gmail.com>
2021-07-22 15:38:42 -0700

    finished quiz + pillars

--- CrushingCollege/CrushingCollege/PillarsViewController.swift
+++ CrushingCollege/CrushingCollege/PillarsViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class PillarsViewController: UIViewController {
     
+    var editedArray1: [Int] = []
+    
     
     @IBOutlet weak var help1: UILabel!
     
@@ -22,12 +24,99 @@ class PillarsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("Inside Pillar View \(editedArray1)")
+        
         help1.transform = CGAffineTransform(rotationAngle: (5 * CGFloat.pi) / 3)
         help2.transform = CGAffineTransform(rotationAngle: (5 * CGFloat.pi) / 3)
         help3.transform = CGAffineTransform(rotationAngle: (5 * CGFloat.pi) / 3)
         help4.transform = CGAffineTransform(rotationAngle: (5 * CGFloat.pi) / 3)
         help5.transform = CGAffineTransform(rotationAngle: (5 * CGFloat.pi) / 3)
+        
+            if editedArray1[0] == testVal {
+                help5.text = "SAT/ACT Prep"
+            }
+            else if editedArray1[0] == essayVal {
+                help5.text = "Essay Help"
+            }
+            else if editedArray1[0] == aidVal {
+                help5.text = "Fin Aid"
+            }
+            else if editedArray1[0] == collegeVal {
+                help5.text = "College Lists"
+            }
+            else if editedArray1[0] == appsVal {
+                help5.text = "College Apps"
+            }
+        ///------------------------------------------------------
+            if editedArray1[1] == testVal {
+                help4.text = "SAT/ACT Prep"
+            }
+            else if editedArray1[1] == essayVal {
+                help4.text = "Essay Help"
+            }
+            else if editedArray1[1] == aidVal {
+                help4.text = "Fin Aid"
+            }
+            else if editedArray1[1] == collegeVal {
+                help4.text = "College Lists"
+            }
+            else if editedArray1[1] == appsVal {
+                help4.text = "College Apps"
+            }
+        ///------------------------------------------------------
+            if editedArray1[2] == testVal {
+                help3.text = "SAT/ACT Prep"
+            }
+            else if editedArray1[2] == essayVal {
+                help3.text = "Essay Help"
+            }
+            else if editedArray1[2] == aidVal {
+                help3.text = "Fin Aid"
+            }
+            else if editedArray1[2] == collegeVal {
+                help3.text = "College Lists"
+            }
+            else if editedArray1[2] == appsVal {
+                help3.text = "College Apps"
+            }
+        ///------------------------------------------------------
+            if editedArray1[3] == testVal {
+                help2.text = "SAT/ACT Prep"
+            }
+            else if editedArray1[3] == essayVal {
+                help2.text = "Essay Help"
+            }
+            else if editedArray1[3] == aidVal {
+                help2.text = "Fin Aid"
+            }
+            else if editedArray1[3] == collegeVal {
+                help2.text = "College Lists"
+            }
+            else if editedArray1[3] == appsVal {
+                help2.text = "College Apps"
+            }
+        ///------------------------------------------------------
+            if editedArray1[4] == testVal {
+                help1.text = "SAT/ACT Prep"
+            }
+            else if editedArray1[4] == essayVal {
+                help1.text = "Essay Help"
+            }
+            else if editedArray1[4] == aidVal {
+                help1.text = "Fin Aid"
+            }
+            else if editedArray1[4] == collegeVal {
+                help1.text = "College Lists"
+            }
+            else if editedArray1[4] == appsVal{
+                help1.text = "College Apps"
+            }
+        ///------------------------------------------------------
 
+    
+        
+        
         // Do any additional setup after loading the view.
     }
     
--- CrushingCollege/CrushingCollege/QuizViewController.swift
+++ CrushingCollege/CrushingCollege/QuizViewController.swift
@@ -7,8 +7,174 @@
 
 import UIKit
 
+    var testVal = 0
+    var essayVal = 0
+    var aidVal = 0
+    var collegeVal = 0
+    var appsVal = 0
+
+    var quizArray: [Int] = [0,0,0,0,0]
+    
 class QuizViewController: UIViewController {
 
+    var editedArray: [Int] = [0,0,0,0,0]
+    
+    @IBAction func Test1(_ sender: Any) {
+        testVal = 1
+        quizArray[0] = testVal
+    }
+    
+    
+    @IBAction func Test2(_ sender: Any) {
+        testVal = 2
+        quizArray[0] = testVal
+    }
+    
+    
+    @IBAction func Test3(_ sender: Any) {
+        testVal = 3
+        quizArray[0] = testVal
+    }
+    
+    
+    @IBAction func Test4(_ sender: Any) {
+        testVal = 4
+        quizArray[0] = testVal
+    }
+    
+    @IBAction func Test5(_ sender: Any) {
+        testVal = 5
+        quizArray[0] = testVal
+    }
+    
+    @IBAction func Essay1(_ sender: Any) {
+        essayVal = 1
+        quizArray[1] = essayVal
+    }
+    
+    @IBAction func Essay2(_ sender: Any) {
+        essayVal = 2
+        quizArray[1] = essayVal
+    }
+    
+    @IBAction func Essay3(_ sender: Any) {
+        essayVal = 3
+        quizArray[1] = essayVal
+    }
+    
+    @IBAction func Essay4(_ sender: Any) {
+        essayVal = 4
+        quizArray[1] = essayVal
+    }
+    
+    @IBAction func Essay5(_ sender: Any) {
+        essayVal = 5
+        quizArray[1] = essayVal
+    }
+    
+    @IBAction func Aid1(_ sender: Any) {
+        aidVal = 1
+        quizArray[2] = aidVal
+    }
+    
+    @IBAction func Aid2(_ sender: Any) {
+        aidVal = 2
+        quizArray[2] = aidVal
+    }
+    
+    @IBAction func Aid3(_ sender: Any) {
+        aidVal = 3
+        quizArray[2] = aidVal
+    }
+    
+    @IBAction func Aid4(_ sender: Any) {
+        aidVal = 4
+        quizArray[2] = aidVal
+    }
+    
+    @IBAction func Aid5(_ sender: Any) {
+        aidVal = 5
+        quizArray[2] = aidVal
+    }
+    
+    @IBAction func College1(_ sender: Any) {
+        collegeVal = 1
+        quizArray[3] = collegeVal
+    }
+    
+    @IBAction func College2(_ sender: Any) {
+        collegeVal = 2
+        quizArray[3] = collegeVal
+    }
+    
+    @IBAction func College3(_ sender: Any) {
+        collegeVal = 3
+        quizArray[3] = collegeVal
+    }
+    
+    @IBAction func College4(_ sender: Any) {
+        collegeVal = 4
+        quizArray[3] = collegeVal
+    }
+    
+    @IBAction func College5(_ sender: Any) {
+        collegeVal = 5
+        quizArray[3] = collegeVal
+    }
+    
+    @IBAction func Apps1(_ sender: Any) {
+        appsVal = 1
+        quizArray[4] = appsVal
+    }
+    
+    @IBAction func Apps2(_ sender: Any) {
+        appsVal = 2
+        quizArray[4] = appsVal
+    }
+    
+    @IBAction func Apps3(_ sender: Any) {
+        appsVal = 3
+        quizArray[4] = appsVal
+    }
+    
+    @IBAction func Apps4(_ sender: Any) {
+        appsVal = 4
+        quizArray[4] = appsVal
+    }
+    
+    @IBAction func Apps5(_ sender: Any) {
+        appsVal = 5
+        quizArray[4] = appsVal
+        print("Within Apps5 Quiz Array \(quizArray)")
+    }
+    
+    
+    
+    func bubbleSort(arry : [Int]) -> [Int]{
+        print("input \(arry)")
+        
+        var tmp = 0
+        var arrayEdt: [Int] = [0,0,0,0,0]
+    
+        for i in 0..<arry.count {
+          for j in 1..<arry.count {
+            if arry[i] < arry[j] {
+              tmp = arry[j]
+              arrayEdt[j - 1] = arry[i]
+              arrayEdt[j] = tmp
+            }
+          }
+        }
+        print("Within bubble sort \(arrayEdt)")
+        return arrayEdt
+}
+    
+    @IBAction func submit(_ sender: Any) {
+        editedArray = bubbleSort(arry : quizArray)
+        print("Quiz Array \(quizArray) Edited Array \(editedArray)")
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -16,14 +182,17 @@ class QuizViewController: UIViewController {
     }
     
 
-    /*
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.destination is PillarsViewController {
+            editedArray = bubbleSort(arry : quizArray)
+            print("Edited Array in prepare \(editedArray)")
+            let vc = segue.destination as? PillarsViewController
+            vc?.editedArray1 = editedArray
+        }
     }
-    */
 
 }
